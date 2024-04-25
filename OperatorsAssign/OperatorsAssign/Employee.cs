f3a80a58fbdc204650186a5a36b230ea7ff08b60
anishvincent05 <anishvincent@outlook.com>
2024-04-25 10:17:30 -0600

    Operator assign

---
+++ OperatorsAssign/OperatorsAssign/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperatorsAssign
+{
+   public class Employee
+    {
+       //Declaring properties
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //Overloading comparing operator
+        public static bool operator== (Employee Emp1,Employee Emp2)
+        {
+            if (Emp1.ID==Emp2.ID && Emp1.FirstName==Emp2.FirstName && Emp1.LastName == Emp2.LastName)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        //Overloading comparing operator pair
+        public static bool operator !=(Employee Emp1, Employee Emp2)
+        {
+            if (Emp1.ID != Emp2.ID || (Emp1.FirstName != Emp2.FirstName && Emp1.LastName != Emp2.LastName))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ OperatorsAssign/OperatorsAssign/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OperatorsAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Emp1 = new Employee() { ID = 01 ,FirstName="Anish",LastName="Vincent"};//Employee 1
+            Employee Emp2 = new Employee() { ID = 02, FirstName = "Alwyn", LastName = "Vincent" };//Employee 2
+
+            //Comparing Employees 
+            if (Emp1==Emp2)
+            {
+                Console.WriteLine("Same Employees");
+
+            }
+            else
+            {
+                Console.WriteLine("Not Same Employees");
+            }
+            
+
+        }
+    }
+}
