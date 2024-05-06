4b50c812ff981d1f0b48952822e555ce8f3137ea
anishvincent05 <anishvincent@outlook.com>
2024-05-06 09:03:39 -0600

    cf

---
+++ CodeFirstStudent/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstStudent
+{
+    public class Student
+    {
+        public int? StudentID { get; set; }
+        public string StudentName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+
+        public Grade Grade { get; set; }
+    }
+    public class Grade
+    {
+        public int GradeId { get; set; }
+        public string GradeName { get; set; }
+        public string Section { get; set; }
+
+        public ICollection<Student> Student { get; set; }
+    }
+    public class SchoolContext : System.Data.Entity.DbContext
+    {
+        public SchoolContext() : base()
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Bill" };
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
