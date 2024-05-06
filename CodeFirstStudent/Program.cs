ba5ee2c4e53b2618d8ce5d2d5a9dbdcf5da4354b
anishvincent05 <anishvincent@outlook.com>
2024-05-06 08:15:29 -0600

    cf

---
+++ CodeFirstStudent/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace CodeFirstStudent
+{
+    class Program
+    {
+      
+       
+       
+
+        static void Main(string[] args)
+        {
+
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Bill" };
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
