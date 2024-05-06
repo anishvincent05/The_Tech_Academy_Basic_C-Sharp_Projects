6d20c3a1e56b507dabb2694bde3825133bfebc6b
anishvincent05 <anishvincent@outlook.com>
2024-05-06 08:24:53 -0600

    cf

--- CodeFirstStudent/Student.cs
+++ CodeFirstStudent/Student.cs
@@ -8,7 +8,7 @@ namespace CodeFirstStudent
 {
     public class Student
     {
-        public int StudentID { get; set; }
+        public int? StudentID { get; set; }
         public string StudentName { get; set; }
         public DateTime? DateOfBirth { get; set; }
 
